886d1f9e71e93e01b89c1f49a98e697c7d9313cb
nordlead2005 <nord.lead.2005@gmail.com>
2020-03-20 08:19:17 -0400

    Fix migration
    can't have two operations per version

--- src/main/java/com/github/hornta/race/api/migrations/StartOrderMigration.java
+++ src/main/java/com/github/hornta/race/api/migrations/StartOrderMigration.java
@@ -10,16 +10,16 @@ import org.bukkit.configuration.file.YamlConfiguration;
 public class StartOrderMigration implements IFileMigration {
   @Override
   public RaceVersion from() {
-    return RaceVersion.V11;
-  }
-
-  @Override
-  public RaceVersion to() {
     return RaceVersion.V12;
   }
 
   @Override
+  public RaceVersion to() {
+    return RaceVersion.V13;
+  }
+
+  @Override
   public void migrate(YamlConfiguration yamlConfiguration) {
-    yamlConfiguration.set(FileAPI.START_ORDER_FIELD, StartOrder.RANDOM);
+    yamlConfiguration.set(FileAPI.START_ORDER_FIELD, StartOrder.RANDOM.name());
   }
 }
